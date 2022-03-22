3b786c0968f558ebc1061b5c13bce7465766e6ee
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2022-03-22 21:19:36 +0000

    Implemented sign out on access_token expiration

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -9,6 +9,18 @@ export function useAuth(
 
     // First we need an access token, try to get one from local storage
     let accessToken = window.localStorage.getItem('access_token') ?? '';
+    let accessTokenExpiration = window.localStorage.getItem('access_token_expiration') ?? '';
+
+    // If an available access token has expired, delete it as it won't work with the Spotify API
+    if (
+      accessTokenExpiration
+      && new Date().getTime() > parseInt(accessTokenExpiration)
+    ) {
+      accessToken = '';
+      accessTokenExpiration = '';
+      window.localStorage.removeItem('access_token');
+      window.localStorage.removeItem('access_token_expiration');
+    }
 
     // If there's no available access token, check to see if the URL contains a hash
     // Spotify's implicit grant auth sends an access token via a hash in the URL
@@ -18,11 +30,17 @@ export function useAuth(
       // If there's a hash, attempt to get access token from it then delete the hash
       if (hash) {
         accessToken = new URLSearchParams(hash.substr(1)).get('access_token') ?? '';
+        accessTokenExpiration = new URLSearchParams(hash.substr(1)).get('expires_in') ?? '';
         window.history.replaceState('', document.title, window.location.pathname + window.location.search);
 
         // If we successfully obtained an access token, update local storage
-        if (accessToken) {
+        if (accessToken && accessTokenExpiration) {
           window.localStorage.setItem('access_token', accessToken);
+          // We need to set a timestamp for when the token expires
+          // Spotify provides a string describing number of seconds til expiration
+          // We need to convert this to a unix timestamp for milliseconds from now
+          const unixExpiration = new Date().getTime() + (parseInt(accessTokenExpiration) * 1000);
+          window.localStorage.setItem('access_token_expiration', unixExpiration.toString());
         }
       }
     }
