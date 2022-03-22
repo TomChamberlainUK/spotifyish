c5daf2b1293764e5a79309f901c4ab71128d6051
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2022-03-22 15:21:52 +0000

    Added header, removed centering, fixed incorrect component name

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -6,15 +6,18 @@ type Props = {
   signOut: () => void;
 }
 
-export default function Auth({ user, signOut }: Props) {
+export default function Profile({ user, signOut }: Props) {
   return (
-    <div className={styles.container}>
-      <img className={styles.profilePicture} src={user.imageUrl} alt={`Spotify profile for ${user.name}`}/>
-      <div className={styles.text}>
-        <h2 className={styles.userName}>{user.name}</h2>
-        <p>Welcome Back!</p>
-        <button onClick={signOut}>Sign Out</button>
+    <>
+      <h2>Profile</h2>
+      <div className={styles.container}>
+        <img className={styles.profilePicture} src={user.imageUrl} alt={`Spotify profile for ${user.name}`}/>
+        <div className={styles.text}>
+          <h3 className={styles.userName}>{user.name}</h3>
+          <p>Welcome Back!</p>
+          <button onClick={signOut}>Sign Out</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
